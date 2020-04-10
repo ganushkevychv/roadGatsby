dfcb578902639edf518d61edeaa9d5d1e23f42d3
Vladyslav <ganushkevychv@gmail.com>
2020-04-10 11:19:46 +0200

    new project created

---
+++ src/pages/index.js
@@ -0,0 +1,71 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Button, Card, CardContent, Typography } from '@material-ui/core';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Layout from "../components/layout"
+import "../styles/layout.scss"
+import SEO from "../components/seo"
+import StickyFooter from "../components/footer"
+
+const IndexPage = () => (
+  <Layout>
+    <div className="container-card">
+    <Card variant="outlined" 
+    style={{
+      backgroundColor: "transparent",
+      border:"3px solid grey",
+      borderRightStyle: "none",
+      borderLeftStyle: "none",
+      }}>
+      <CardContent>
+        <Typography  color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+        zvsdsdfddsfsdf
+        </Typography>
+        <Typography  color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+        </Typography>
+      </CardContent>
+    </Card>
+    </div>
+    <div className="container-button">
+    <ButtonGroup color="primary" aria-label="outlined primary button group" 
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      
+      }} >
+  <Button style={{
+    border:"3px solid grey",
+  }}><Link to="/page-2/"  
+  style={{
+    textDecoration:"none",
+    color:"black"
+    }}>О нас</Link></Button>
+  <Button style={{
+    border:"3px solid grey",
+  }} ><Link to="/page-2/"  
+  style={{
+    textDecoration:"none",
+    color:"black"
+    }}>Информация</Link></Button>
+  <Button style={{
+    border:"3px solid grey",
+  }}><Link to="/page-2/"  
+  style={{
+    textDecoration:"none",
+    color:"black"
+    }}>Контакты</Link></Button>
+</ButtonGroup>
+</div>
+    <StickyFooter/>
+  </Layout>
+)
+
+export default IndexPage
