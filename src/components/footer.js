ffb76844d73e1a0b316b999d00dc4e0052d14533
Vladyslav <ganushkevychv@gmail.com>
2020-04-13 14:00:37 +0200

    new styles added to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,52 +1,17 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
+import "../styles/footer.scss"
+import Logo from "../images/me.png"
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary">
-      {'Copyright © '}
-      <Link color="inherit" href="https:\\vladyslav-ganushkevych.netlify.com">
-        VG
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
-  },
-  main: {
-    marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2),
-  },
-  footer: {
-    padding: theme.spacing(3, 2),
-    marginTop: 'auto',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
-  },
-}));
-
-export default function StickyFooter() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <footer className={classes.footer}>
-        <Container maxWidth="sm">
-          <Copyright />
-        </Container>
-      </footer>
-    </div>
-  );
-}
+const footer = () => {
+  return(
+      <div className="site-footer">
+      <h4 className="text-center">All rights reserved© {new Date().getFullYear()}, Created by
+            {` `}
+            <a href="https://vladyslav-ganushkevych.netlify.com/"><img src={Logo} width="30" 
+    height="30" alt="logo"/></a></h4>
+      
+     
+      </div>
+  )
+  }
+  export default footer
