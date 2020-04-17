d3c15a8a66dd53afeec8c26eee535a1e3b19dbc5
Vladyslav <ganushkevychv@gmail.com>
2020-04-17 16:53:00 +0200

    description text added to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Ремонт дорог – наше призвание!
+    Считается, что положить асфальт – элементарный процесс. Но чтобы сделать это правильно и качественно, нужно обратиться к нам.`,
     author: `@gatsbyjs`,
   },
   plugins: [
