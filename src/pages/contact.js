baa47d562a505be10531606ee38721e119a075ad
Vladyslav <ganushkevychv@gmail.com>
2020-04-17 11:34:27 +0200

    new cards added to tech page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { Button } from '@material-ui/core'
 import { Link } from "gatsby"
-import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Contact = () => (
-  <Layout>
+  <div className="container">
     <SEO title="contact" />
     <h1>Контакты</h1>
+    <div className="contact-card">
+      
+    </div>
     <Button style={{
     border:"3px solid grey",
   }}><Link to="/"  
@@ -15,7 +17,7 @@ const Contact = () => (
     textDecoration:"none",
     color:"white"
     }}>Назад</Link></Button>
-  </Layout>
+  </div>
 )
 
 export default Contact
--- src/pages/tech.js
+++ src/pages/tech.js
@@ -7,8 +7,11 @@ import loader from "../images/loader.jpg"
 import bulldozer from "../images/bulldozer.jpg"
 import excavator from "../images/excavator.png"
 import maz from "../images/maz.jpg"
+import carTrailer from "../images/carTrailer.jpg"
+import stuff from "../images/stuff.jpg"
 import mini from "../images/mini.png"
-import ford from "../images/ford.png"
+import roller from "../images/roller.png"
+import ford from "../images/ford.jpg"
 import dumper from "../images/dumper.jpg"
 import reno from "../images/reno.jpg"
 import trailer from "../images/trailer.jpg"
@@ -208,7 +211,76 @@ backgroundColor:"rgba(0,0,0,0.4)"
     <Card.Body>
       <Card.Title>Думпер</Card.Title>
       <Card.Text>
-      описание
+      Aures G400
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button style={{
+    backgroundColor:"#90caf9",
+    textAlign:"center"
+  }}><Link to="/contact/"  
+  style={{
+    textDecoration:"none",
+    
+    }}>Заказать</Link></Button>
+    </Card.Footer>
+  </Card>
+  </CardDeck>
+</div>
+<div className="cards-four">
+  
+  <CardDeck>
+<Card style={{ width: '18rem',
+backgroundColor:"rgb(0,0,0)",
+backgroundColor:"rgba(0,0,0,0.4)"
+ }} >
+    <Card.Img variant="top" src={carTrailer} style={{width:"100%"}} />
+    <Card.Body>
+      <Card.Title>Прицеп</Card.Title>
+      <Card.Text >
+      Грузоподъемность 2 тонны
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button style={{
+    backgroundColor:"#90caf9"
+  }}><Link to="/contact/"  
+  style={{
+    textDecoration:"none",
+    color:""
+    }}>Заказать</Link></Button>
+    </Card.Footer>
+  </Card>
+  <Card style={{ width: '18rem',
+backgroundColor:"rgb(0,0,0)",
+backgroundColor:"rgba(0,0,0,0.4)"
+}} >
+    <Card.Img variant="top" src={roller} style={{width:"100%"}} />
+    <Card.Body>
+      <Card.Title>Каток</Card.Title>
+      <Card.Text>
+      Ford Transit с прицепом
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Button style={{
+    backgroundColor:"#90caf9"
+  }}><Link to="/contact/"  
+  style={{
+    textDecoration:"none",
+    color:""
+    }}>Заказать</Link></Button>
+    </Card.Footer>
+  </Card>
+  <Card style={{ width: '18rem',
+backgroundColor:"rgb(0,0,0)",
+backgroundColor:"rgba(0,0,0,0.4)"
+}} >
+    <Card.Img variant="top" src={stuff} style={{width:"100%"}} />
+    <Card.Body>
+      <Card.Title>Персонал</Card.Title>
+      <Card.Text>
+      Персонал с опытом
       </Card.Text>
     </Card.Body>
     <Card.Footer>
